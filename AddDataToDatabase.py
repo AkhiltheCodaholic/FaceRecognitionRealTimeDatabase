5496801a38f707ba53073cbd7e1595e80903437d
AkhiltheCodaholic <akhil.s1414@gmail.com>
2024-01-04 15:30:01 +0530

    First Commit
    first facial recognization project
    with Python

---
+++ AddDataToDatabase.py
@@ -0,0 +1,58 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://faceattendancerealtime-e0b45-default-rtdb.firebaseio.com/",
+    'storageBucket': "faceattendancerealtime-e0b45.appspot.com"
+})
+
+ref = db.reference("Students")
+
+data = {
+    "192947":
+        {
+            "name": "Akhil Saketh",
+            "major": "student",
+            "starting_year": 2021,
+            "total_attendance": 6,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+
+    "321654":
+        {
+            "name": "Murtaza Hassan",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emily Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "G",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key,value in data.items():
+    ref.child(key).set(value)
